c54e8e658cf57dd3a1414e9d478f17d482aa58f7
Alexander <m.mirvua@gmail.com>
2024-07-06 08:25:58 +0300

    finished details page;

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_travel_v2_app/model/hotel.dart';
 import 'package:flutter_travel_v2_app/style/project_colors.dart';
 import 'package:flutter_travel_v2_app/widgets/buttons/circled_button.dart';
 import 'package:flutter_travel_v2_app/widgets/buttons/custom_text_button.dart';
+import 'package:flutter_travel_v2_app/widgets/buttons/splash_button.dart';
 import 'package:flutter_travel_v2_app/widgets/custom_badge.dart';
 import 'package:flutter_travel_v2_app/widgets/hotel_item/location_row.dart';
 import 'package:flutter_travel_v2_app/widgets/hotel_item/price_row.dart';
@@ -124,196 +125,205 @@ class _DetailsPageState extends State<DetailsPage> {
                   ),
                   color: Colors.white,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
 
-                    const Gap(20.0),
+                      const Gap(20.0),
 
-                    //hotel name
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: PrimaryText(
-                        text: widget.hotel.name,
+                      //hotel name
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: PrimaryText(
+                          text: widget.hotel.name,
+                        ),
                       ),
-                    ),
 
-                    const Gap(5.0),
+                      const Gap(5.0),
 
-                    //location
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: LocationRow(
-                        locationName: widget.hotel.location,
-                        color: Colors.black87,
+                      //location
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: LocationRow(
+                          locationName: widget.hotel.location,
+                          color: Colors.black87,
+                        ),
                       ),
-                    ),
 
-                    //stars and price row
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
+                      //stars and price row
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
 
-                          //stars & reviews
-                          StarsRow(
-                            stars: widget.hotel.stars.toString(),
-                            fontSize: 16,
-                            iconSize: 24,
-                            textColor: null,
-                            reviews: widget.hotel.reviews.toInt(),
-                          ),
-
-                          PriceRow(
-                            price: widget.hotel.price.toString(),
-                            textColor: null,
-                            priceFontSize: 20,
-                            labelFontSize: 16,
-                          )
-                        ],
-                      ),
-                    ),
-
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
-                      child: Divider(
-                        color: Colors.black12,
-                      ),
-                    ),
-
-                    //description
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: Text(
-                        widget.hotel.description,
-                        maxLines: !isDescriptionExpanded ? 3 : 10,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-
-                    //'read more' button
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25.0),
-                      child: CustomTextButton(
-                        text: !isDescriptionExpanded ? 'Read More' : 'Read Less',
-                        fontSize: null,
-                        onTap: () {
-                          setState(() {
-                            isDescriptionExpanded = !isDescriptionExpanded;
-                          });
-                        },
-                      ),
-                    ),
-
-                    const Gap(30.0),
-
-                    //'What we offer' section title
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25.0),
-                      child: SectionTitle(
-                        title: 'What we offer',
-                      ),
-                    ),
-
-                    const Gap(10.0),
-
-                    //'services offered' row
-                    Row(
-                      children: List.generate(widget.hotel.services.length, (index) {
-
-                        return ServiceItem(
-                          label: widget.hotel.services.keys.elementAt(index),
-                          icon: widget.hotel.services.values.elementAt(index),
-                        );
-
-                      }),
-                    ),
-
-                    const Gap(30.0),
-
-                    //'Hosted by' section title
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25.0),
-                      child: SectionTitle(
-                        title: 'Hosted by',
-                      ),
-                    ),
-
-                    const Gap(10.0),
-
-                    //'Hosted by' section
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: Row(
-                        children: [
-
-                          //owner photo
-                          Container(
-                            height: 45,
-                            width: 45,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0),
-                              color: Colors.black12,
+                            //stars & reviews
+                            StarsRow(
+                              stars: widget.hotel.stars.toString(),
+                              fontSize: 16,
+                              iconSize: 24,
+                              textColor: null,
+                              reviews: widget.hotel.reviews.toInt(),
                             ),
-                          ),
 
-                          const Gap(10.0),
-
-                          //owner details
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-
-                                PrimaryText(
-                                  fontSize: 16,
-                                  text: widget.hotel.ownerName,
-                                ),
-
-                                StarsRow(
-                                  stars: widget.hotel.ownerStars.toString(),
-                                  textColor: Colors.black87,
-                                  reviews: widget.hotel.ownerReviews.toInt(),
-                                ),
-
-                              ],
-                            ),
-                          ),
-
-                          const Gap(10.0),
-
-                          //message button
-                          Container(
-                            height: 45,
-                            width: 45,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0),
-                              color: ProjectColors.accentColor,
-                              boxShadow: [
-                                BoxShadow(
-                                  offset: const Offset(2, 10.0),
-                                  blurRadius: 10.0,
-                                  color: ProjectColors.accentColor.withOpacity(0.3),
-                                ),
-                              ],
-                            ),
-                            child: const Icon(
-                              Icons.message,
-                              color: Colors.white,
-                            ),
-                          ),
-
-                        ],
+                            PriceRow(
+                              price: widget.hotel.price.toString(),
+                              textColor: null,
+                              priceFontSize: 20,
+                              labelFontSize: 16,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
 
-                    //'Book now' bottom button
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+                        child: Divider(
+                          color: Colors.black12,
+                        ),
+                      ),
+
+                      //description
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Text(
+                          widget.hotel.description,
+                          maxLines: !isDescriptionExpanded ? 3 : 10,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+
+                      //'read more' button
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
+                        child: CustomTextButton(
+                          text: !isDescriptionExpanded ? 'Read More' : 'Read Less',
+                          fontSize: null,
+                          onTap: () {
+                            setState(() {
+                              isDescriptionExpanded = !isDescriptionExpanded;
+                            });
+                          },
+                        ),
+                      ),
+
+                      const Gap(30.0),
+
+                      //'What we offer' section title
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25.0),
+                        child: SectionTitle(
+                          title: 'What we offer',
+                        ),
+                      ),
+
+                      const Gap(10.0),
+
+                      //'services offered' row
+                      Row(
+                        children: List.generate(widget.hotel.services.length, (index) {
+
+                          return ServiceItem(
+                            label: widget.hotel.services.keys.elementAt(index),
+                            icon: widget.hotel.services.values.elementAt(index),
+                          );
+
+                        }),
+                      ),
+
+                      const Gap(30.0),
+
+                      //'Hosted by' section title
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25.0),
+                        child: SectionTitle(
+                          title: 'Hosted by',
+                        ),
+                      ),
+
+                      const Gap(10.0),
+
+                      //'Hosted by' section
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Row(
+                          children: [
+
+                            //owner photo
+                            Container(
+                              height: 45,
+                              width: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0),
+                                color: Colors.black12,
+                              ),
+                            ),
+
+                            const Gap(10.0),
+
+                            //owner details
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+
+                                  PrimaryText(
+                                    fontSize: 16,
+                                    text: widget.hotel.ownerName,
+                                  ),
+
+                                  StarsRow(
+                                    stars: widget.hotel.ownerStars.toString(),
+                                    textColor: Colors.black87,
+                                    reviews: widget.hotel.ownerReviews.toInt(),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+
+                            const Gap(10.0),
+
+                            //message button
+                            Container(
+                              height: 45,
+                              width: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0),
+                                color: ProjectColors.accentColor,
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: const Offset(2, 10.0),
+                                    blurRadius: 10.0,
+                                    color: ProjectColors.accentColor.withOpacity(0.3),
+                                  ),
+                                ],
+                              ),
+                              child: const Icon(
+                                Icons.message,
+                                color: Colors.white,
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+
+                      const Gap(30.0),
+
+                      //'Book now' bottom button
+                      SplashButton(
+                        text: 'Book Now',
+                        onTap: () {},
+                      ),
+
+                      const Gap(20.0),
 
 
-
-                  ],
+                    ],
+                  ),
                 ),
               )
             )
