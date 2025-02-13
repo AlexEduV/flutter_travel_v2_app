6c4a6df9842fd40944a5942e91f52049a13a60c4
Alexander Aks'onov <m.mirvua@gmail.com>
2025-02-13 16:54:55 +0200

    clean up;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_travel_v2_app/model/hotel.dart';
-import 'package:flutter_travel_v2_app/pages/details_page.dart';
-import 'package:flutter_travel_v2_app/pages/home_page.dart';
-import 'package:flutter_travel_v2_app/pages/discover_page.dart';
 import 'package:flutter_travel_v2_app/router/router.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
