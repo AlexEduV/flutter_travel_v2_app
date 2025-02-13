6615b32003aee8573dc35ff5adca0b1d70d5b03c
Alexander Aks'onov <m.mirvua@gmail.com>
2025-02-13 16:54:07 +0200

    moved router configuration to the separate file;

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,12 @@ import 'package:flutter_travel_v2_app/model/hotel.dart';
 import 'package:flutter_travel_v2_app/pages/details_page.dart';
 import 'package:flutter_travel_v2_app/pages/home_page.dart';
 import 'package:flutter_travel_v2_app/pages/discover_page.dart';
+import 'package:flutter_travel_v2_app/router/router.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-//go router configuration
-final GoRouter _router = GoRouter(
-  initialLocation: '/',
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      name: 'root',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomePage();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'discover',
-          name: 'discover',
-          builder: (BuildContext context, GoRouterState state) {
-
-            return const DiscoverPage();
-          },
-          routes: <RouteBase>[
-            GoRoute(
-              path: 'details',
-              name: 'details',
-              builder: (BuildContext context, GoRouterState state) {
-                Hotel hotel = state.extra as Hotel;
-                
-                return DetailsPage(hotel: hotel);
-              }
-            )
-          ],
-        ),
-      ],
-    ),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -57,7 +23,7 @@ class MyApp extends StatelessWidget {
     ]);
 
     return MaterialApp.router(
-      routerConfig: _router,
+      routerConfig: CustomRouterConfig.router,
       title: 'Flutter Travel App',
       theme: ThemeData(
         useMaterial3: true,
---
+++ lib/router/router.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_travel_v2_app/model/hotel.dart';
+import 'package:flutter_travel_v2_app/pages/details_page.dart';
+import 'package:flutter_travel_v2_app/pages/discover_page.dart';
+import 'package:flutter_travel_v2_app/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomRouterConfig {
+
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        name: 'root',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomePage();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'discover',
+            name: 'discover',
+            builder: (BuildContext context, GoRouterState state) {
+
+              return const DiscoverPage();
+            },
+            routes: <RouteBase>[
+              GoRoute(
+                  path: 'details',
+                  name: 'details',
+                  builder: (BuildContext context, GoRouterState state) {
+                    Hotel hotel = state.extra as Hotel;
+
+                    return DetailsPage(hotel: hotel);
+                  }
+              )
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+
+}
